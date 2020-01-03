d8dfd0c606c255d438b29ce4576f0735f51cb2ae
jimi <1652877739@qq.com>
2020-01-04 01:27:50 +0800

    gmall-api

---
+++ gmall-mbg/src/main/java/com/atguigu/gmall/ums/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.ums.mapper;
+
+import com.atguigu.gmall.ums.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 后台用户角色表 Mapper 接口
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-01-04
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
